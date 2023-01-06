c9fea148088bedd83e15bf14642d5d90f9ef1280
Leonardo Brito Bittencourt <leonardobritobittencourt@gmail.com>
2023-01-06 18:26:17 -0300

    fix: clean logs

--- src/application/use-case/deposit-to-a-client.use-case.ts
+++ src/application/use-case/deposit-to-a-client.use-case.ts
@@ -24,7 +24,6 @@ export class DepositToAClientUseCase {
     let jobsToPay = jobs.reduce((total, { price }) => (total += price), 0);
     const percentageAllowed = 1.25;
     const depositLimit = jobsToPay * percentageAllowed;
-    console.log("🚀 ~ file: deposit-to-a-client.use-case.ts:27 ~ DepositToAClientUseCase ~ execute ~ depositLimit", depositLimit);
 
     if (amount > depositLimit) {
       throw new Error("You can't deposit more than 25% of the total of jobs you have to pay");
